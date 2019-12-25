6f9b9eccda9deee66238bb23a4e0e4b9b43c2c64
Maxfragov <51209740+Maxfragov@users.noreply.github.com>
2019-12-25 23:14:07 +0300

    Add files via upload

---
+++ INDIV2/f.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+X = np.array([
+    [-2,4,-1],
+    [4,1,-1],
+    [1, 6, -1],
+    [2, 4, -1],
+    [6, 2, -1],
+
+])
+
+y = np.array([-1,-1,1,1,1])
+
+def perceptron_sgd(X, Y):
+    w = np.zeros(len(X[0]))
+    eta = 1
+    epochs = 20
+
+    for t in range(epochs):
+        for i, x in enumerate(X):
+            if (np.dot(X[i], w)*Y[i]) <= 0:
+                w = w + eta*X[i]*Y[i]
+
+    return w
+
+
+def perceptron_sgd_plot(X, Y):
+    '''
+    train perceptron and plot the total loss in each epoch.
+
+    :param X: data samples ВХОД
+    :param Y: data labels  ВЫХОД
+    :return: weight vector as a numpy array ВЕС
+    '''
+    w = np.zeros(len(X[0]))
+    eta = 1
+    n = 30
+    errors = []
+
+    for t in range(n):
+        total_error = 0
+        for i, x in enumerate(X):
+            if (np.dot(X[i], w) * Y[i]) <= 0:
+                total_error += (np.dot(X[i], w) * Y[i])
+                w = w + eta * X[i] * Y[i]
+        errors.append(total_error * -1)
+
+    plt.plot(errors)
+    plt.xlabel('Epoch')
+    plt.ylabel('Total Loss')
+
+    return w
+
+w = perceptron_sgd(X,y)
+perceptron_sgd_plot(X,y)
+print(w)
